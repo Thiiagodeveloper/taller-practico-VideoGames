f18d5814a50c33d6686b3e1b986c302418e5eae8
Santiago <santiagoloaizagarcia14@gmail.com>
2023-07-21 12:03:35 -0500

    Aprendiendo un poco de Canvas, class 02

--- game.js
+++ game.js
@@ -1,1 +1,15 @@
-console.log(maps);
+const canvas = document.querySelector("#game");
+const game = canvas.getContext("2d");
+
+window.addEventListener("load", starGame);
+
+function starGame() {
+  //   game.fillRect(0, 0, 100, 100);
+  //   game.clearRect(50, 50, 50, 50);
+  //   game.clearRect();
+
+  game.font = "25px Verdana";
+  game.fillStyle = "purple";
+  game.textAlign = "left";
+  game.fillText("Platzi", 50, 50);
+}
